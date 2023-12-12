4ba3135400e9f611cb560306cd3b780f0170114c
lirinton <147742036+lirinton@users.noreply.github.com>
2023-12-12 22:52:16 +0000

    f it

--- js/main.js
+++ js/main.js
@@ -1,70 +1,68 @@
+
 function main() {
-  (function () {
-    'use strict';
 
-    // Smooth scrolling for page links
-    $('a.page-scroll').click(function () {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top - 40
-        }, 900);
-        return false;
-      }
-    });
-
-    // Navbar affix
-    $(function () {
-      var header = $('header');
-      var navbar = $('#nav');
-
-      navbar.affix({
-        offset: {
-          top: header.height()
+(function () {
+   'use strict';
+   
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
         }
       });
 
-      navbar.on('affixed.bs.affix', function () {
-        $('body').addClass('affix-body');
-      });
-
-      navbar.on('affixed-top.bs.affix', function () {
-        $('body').removeClass('affix-body');
-      });
-    });
-
-    // Portfolio isotope filter
-    var $container = $('.portfolio-items');
-    $container.isotope({
-      filter: '*',
-      animationOptions: {
-        duration: 750,
-        easing: 'linear',
-        queue: false
+	// affix the navbar after scroll below header
+$('#nav').affix({
+      offset: {
+        top: $('header').height()
       }
-    });
+});	
 
-    $('.cat a').click(function () {
-      $('.cat .active').removeClass('active');
-      $(this).addClass('active');
-      var selector = $(this).attr('data-filter');
-      $container.isotope({
-        filter: selector,
-        animationOptions: {
-          duration: 750,
-          easing: 'linear',
-          queue: false
-        }
-      });
-      return false;
-    });
+	
+  	// Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
 
-    // Nivo Lightbox
+    });
+	
+
+    // Nivo Lightbox 
     $('.portfolio-item a').nivoLightbox({
-      effect: 'slideDown',
-      keyboardNav: true
-    });
-  })();
-}
+            effect: 'slideDown',  
+            keyboardNav: true,                            
+        });
+ 
 
+}());
+
+
+}
+main();
